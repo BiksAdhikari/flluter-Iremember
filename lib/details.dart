1b16a350a39ea7bfeb84ae6606ce30d3e2be3cf9
BiksAdhikari <badriadhikar645@gmail.com>
2019-03-01 11:54:26 +0545

    first commit

---
+++ lib/details.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+class ItemDetails extends StatelessWidget {
+  final Map item;
+  const ItemDetails({Key key, this.item}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue,
+        title: Text(item["title"]),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            DecoratedBox(
+              child: Container(
+                height: 150,
+                width: 150,
+              ),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                image: FileImage(
+                  item["img"],
+                ),
+              )),
+            ),
+            Text(
+              item["title"],
+              style: Theme.of(context).textTheme.display1,
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(item["description"]),
+            SizedBox(
+              height: 40.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+//import 'package:image_picker/image_picker.dart';
+import './Add.dart';
+import './details.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List item1 = [];
+  adding(String title, String description, File _image) {
+    setState(() {
+      item1.add({"title": title, "description": description, "img": _image});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        backgroundColor: Colors.brown,
+      ),
+      body: ListView.builder(
+        itemCount: item1.length,
+        itemBuilder: (BuildContext context, int index) {
+          var item = item1[index];
+          return ListTile(
+            onTap: () => Navigator.push(context,
+                MaterialPageRoute(builder: (_) => ItemDetails(item: item))),
+            isThreeLine: true,
+            title: Text(item["title"]),
+            leading: CircleAvatar(
+              backgroundImage: FileImage(
+                item["img"],
+              ),
+              radius: 35,
+            ),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(item["description"]),
+                SizedBox(
+                  height: 50,
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.of(context).push(MaterialPageRoute(
+            builder: (BuildContext context) => AddPage(adding))),
+        tooltip: 'Add more item',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
